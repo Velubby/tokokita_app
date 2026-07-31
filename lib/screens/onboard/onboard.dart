c0e562a86eb2732c74fed4eb24a1387ed2a13632
Velubby <ahmad.ichwani86@gmail.com>
2026-07-31 18:19:59 +0700

    feat: v1.0.0 initial release

--- lib/screens/onboard/onboard.dart
+++ lib/screens/onboard/onboard.dart
@@ -38,6 +38,7 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
   }
 
   Widget buildPage({
+    required BuildContext context,
     required Color color,
     required String urlImage,
     required String title,
@@ -45,36 +46,39 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
     Color titleColor = Colors.white,
     Color subtitleColor = Colors.white70,
   }) {
+    final screenHeight = MediaQuery.of(context).size.height;
     return Container(
       color: color,
+      padding: const EdgeInsets.symmetric(horizontal: 32),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Image.asset(
-            urlImage,
-            width: double.infinity,
-            fit: BoxFit.cover,
+          SizedBox(
+            height: screenHeight * 0.32,
+            child: Image.asset(
+              urlImage,
+              fit: BoxFit.contain,
+            ),
           ),
-          const SizedBox(height: 64),
+          const SizedBox(height: 40),
           Text(
             title,
+            textAlign: TextAlign.center,
             style: TextStyle(
               fontFamily: 'Poppins',
-              fontSize: 32,
+              fontSize: 26,
               fontWeight: FontWeight.bold,
               color: titleColor,
             ),
           ),
-          SizedBox(height: 16),
-          Container(
-            padding: const EdgeInsets.symmetric(horizontal: 48),
-            child: Text(
-              subtitle,
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 18,
-                color: subtitleColor,
-              ),
+          const SizedBox(height: 16),
+          Text(
+            subtitle,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 16,
+              color: subtitleColor,
+              height: 1.4,
             ),
           ),
         ],
@@ -97,6 +101,7 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
             },
             children: [
               buildPage(
+                context: context,
                 color: Colors.blue,
                 urlImage: 'assets/images/welcome.png',
                 title: 'Welcome to Toko Kita',
@@ -104,13 +109,14 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
                     'Discover the simple and user-friendly Inventory Management of our app.',
               ),
               buildPage(
+                context: context,
                 color: Colors.white,
                 urlImage: 'assets/images/welcome2.png',
                 title: 'Start Managing Inventory',
                 subtitle:
                     'Don\'t waste your time, create your team and start managing your inventory now.',
                 titleColor: Colors.black,
-                subtitleColor: Colors.black,
+                subtitleColor: Colors.black54,
               ),
             ],
           ),
